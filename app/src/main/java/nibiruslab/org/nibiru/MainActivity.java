892144fbdf3be3bf12a42c379f5b85fc83e54e44
David <davidam612@gmail.com>
2015-01-14 20:53:04 +0100

    database autocomplete

--- app/src/main/java/nibiruslab/org/nibiru/MainActivity.java
+++ app/src/main/java/nibiruslab/org/nibiru/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 
 import com.crashlytics.android.Crashlytics;
 
